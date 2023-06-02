c8b327e2de8b30ba9704a8eeeac614684ecfc628
GitRockie <elromankos@gmail.com>
2023-06-02 02:47:27 +0200

    Add Logout and Show/Hide Nav to App Component
    Add Logic to App Component
    Update Nav in App Component Template
    Add Logout and Show/Hide Nav to App Component
    Update Nav in App Component Template
    Create Auth Guard CanActivateFn
    
    Add Auth Guard to Helpers Barrel File
    Add Auth Guard to Home Page Route
    Create JWT Interceptor
    Add JWT Interceptor to Barrel File
    Create Error Interceptor
    
    Add HTTP Interceptors to App Module
    Add Providers to App Module
    Login with test user

--- src/app/account/login.component.ts
+++ src/app/account/login.component.ts
@@ -55,6 +55,7 @@ export class LoginComponent implements OnInit {
                     this.router.navigateByUrl(returnUrl);
                 },
                 error: (error: any) => {
+                
                     this.error = error;
                     this.loading = false;
                 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule} from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
 
 import { AppRoutingModule } from './app-routing.module';
+import { JwtInterceptor, ErrorInterceptor } from './_helpers';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home';
 import { LoginComponent, RegisterComponent } from './account';
@@ -27,6 +28,9 @@ import { LoginComponent, RegisterComponent } from './account';
     
     ],
     providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+        
         // provider used to create fake backend
         fakeBackendProvider
     ],
