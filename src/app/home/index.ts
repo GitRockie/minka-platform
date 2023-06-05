f01c9fec85386200efc7a398d05e78046817d8ca
GitRockie <elromankos@gmail.com>
2023-06-05 11:12:16 +0200

    Fixture works for futher fusion

--- src/app/home/index.ts
+++ src/app/home/index.ts
@@ -1,3 +1,2 @@
 /*this is a barrel file that re-exports components from the home folder so they can be imported in other files using only the folder path (e.g. './home') instead of the full path to the component (e.g. './home/home.component'). For more info on TypeScript barrel files see https://basarat.gitbook.io/typescript/main-1/barrel.*/
-
 export * from './home.component';
