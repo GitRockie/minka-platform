c92f2bad9ffae1050b079548d520a71883ea2d95
GitRockie <elromankos@gmail.com>
2023-06-16 17:02:43 +0200

    Add Politica

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { authGuard } from './_helpers';
 import { HomeComponent } from "./home/home.component";
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { TypographyComponent } from "./pages";
+import { PoliticaComponent } from "./politica/politica.component";
 
 
 
@@ -17,11 +18,12 @@ const usersRoutes = () => import('./users/users.routes').then(x => x.USERS_ROUTE
 
 
 export const APP_ROUTES: Routes= [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: 'home' },
     { path: 'home', component: HomeComponent, title: 'Inicio'},
     { path: 'users', loadChildren: usersRoutes },
     { path: 'account/login', component: LoginComponent },
     { path: 'account/register', component: RegisterComponent },
+    { path: "politica", component: PoliticaComponent },
 
     { path: "admin", 
       component:AdminLayoutComponent, 
---
+++ src/app/politica/politica.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-politica",
+  standalone: true,
+  templateUrl: "politica.component.html"
+})
+export class PoliticaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
