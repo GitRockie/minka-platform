2cdee8b995550c9e37b8bfb8d5585022857741a6
GitRockie <elromankos@gmail.com>
2023-06-16 20:11:22 +0200

    Politica works

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from "@angular/core";
 import { DatePipe } from "@angular/common";
-
+import { Router, ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: "app-footer",
   standalone: true,
-  imports: [DatePipe],
+  imports: [DatePipe, RouterLink,  ],
   templateUrl: "./footer.component.html",
   styleUrls: ["./footer.component.css"]
 })
 export class FooterComponent implements OnInit {
    today: number =  Date.now();
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit() {}
+
+  goToPolitica() {
+    this.router.navigateByUrl("/politica");
+}
 }
